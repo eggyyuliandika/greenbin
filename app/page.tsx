9d6b5397659bb6606cf878d37e3dfb8184c36bbd
eggyyuliandika <eggyyuliandika@gmail.com>
2024-07-24 21:27:59 +0800

    fixed some error

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,18 @@
 import Header from "@/components/Header";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
 export default async function Index() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data: todos } = await supabase.from("todos").select();
+
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <Header />
       </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
     </div>
   );
 }
