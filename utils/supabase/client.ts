86bd1bc2c4d03b7d1be32980577cb522c9d82db7
eggyyuliandika <eggyyuliandika@gmail.com>
2024-07-25 09:31:36 +0800

    fixed some error

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,16 @@
 import { createBrowserClient } from "@supabase/ssr";
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
-export const createClient = () =>
+// Fungsi untuk menginisialisasi klien Supabase di sisi browser
+export const createBrowserSupabaseClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+// Fungsi untuk menginisialisasi klien Supabase di sisi server
+export const createServerSupabaseClient = () =>
+  createSupabaseClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,12 +1,17 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+const supabaseClient = createSupabaseClient(supabaseUrl, supabaseKey);
 
 export function createClient() {
   const cookieStore = cookies();
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  supabaseUrl, supabaseKey,
     {
       cookies: {
         getAll() {
