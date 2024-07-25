ffcc4d76b8bee9e7d6de59ecadd15b5ee52e2948
eggyyuliandika <eggyyuliandika@gmail.com>
2024-07-25 09:44:51 +0800

    fixed some error

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -11,7 +11,8 @@ export function createClient() {
   const cookieStore = cookies();
 
   return createServerClient(
-  supabaseUrl, supabaseKey,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
