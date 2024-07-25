34164d4089ea67e23ecdb3c55e141b25a7bea422
eggyyuliandika <eggyyuliandika@gmail.com>
2024-07-25 10:22:55 +0800

    fiex some errpr

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,14 +1,14 @@
 import { createBrowserClient } from "@supabase/ssr";
 import { createClient } from "@supabase/supabase-js";
 
-// Fungsi untuk menginisialisasi klien Supabase di sisi browser
+
 export const createBrowserSupabaseClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 
-// Fungsi untuk menginisialisasi klien Supabase di sisi server
+
 export const createServerSupabaseClient = () =>
   createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
